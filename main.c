0aef4625e82596f2a4de884c4bcc418411a0faf4
tensorknower69 <62006583+tensorknower69@users.noreply.github.com>
2020-10-29 17:05:04 +0800

    improve error msg

--- main.c
+++ main.c
@@ -88,7 +88,7 @@ int main(int argc, char **argv) {
 		height = mode->height;
 	} else {
 		if (width == NO_VALUE || height == NO_VALUE) {
-			fprintf(stderr, "width or/and height aren't specified, -h for help\n");
+			fprintf(stderr, "Window width or/and height aren't specified, use -h for help\n");
 			exit(1);
 		}
 		mon = NULL;
